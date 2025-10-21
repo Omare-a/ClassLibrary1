255c666bcaa44dabab192b0c7fa5a8d3b344018d
Ömer <omareany99@hotmail.com>
2025-10-21 02:12:15 +0200

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLibrary1;
+
+public class Class1
+{
+
+    public int Add(int a, int b) 
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int  a, int b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("cannot divide by zero");
+        }
+        return a / b;
+    }
+
+
+}
