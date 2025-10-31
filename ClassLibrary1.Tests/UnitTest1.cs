19bf6ec13fc73a2179c2dbc51959116ada3fb043
Ömer <omareany99@hotmail.com>
2025-10-31 02:43:32 +0100

    skapde en gemensam instans för alla tester

--- ClassLibrary1.Tests/UnitTest1.cs
+++ ClassLibrary1.Tests/UnitTest1.cs
@@ -8,67 +8,61 @@
 
 public class UnitTest1
 {
+    private readonly ClassLibrary1.Class1 _calc;
+
     [Fact]
     public void AddTwoNumbers_ReturnSum()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(5, calc.Add(2, 3));
+        //så kan man också göra = var calc = new ClassLibrary1.Class1();
+        Assert.Equal(5, _calc.Add(2, 3));
     }
 
     [Fact]
     public void AddNegetiveNumber_ReturnSum()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(-5, calc.Add(-2, -3));
+        Assert.Equal(-5, _calc.Add(-2, -3));
     }
 
     [Fact]
     public void SubstractTwoNumber_ReturnsDifference()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(2, calc.Subtract(5, 3));
+        Assert.Equal(2, _calc.Subtract(5, 3));
     }
 
     [Fact]
     public void SubstractNegetiveNumbers_ReturnSum()
     {
-        var clac = new ClassLibrary1.Class1();
-        Assert.Equal(1, clac.Subtract(-2, -3));
+        Assert.Equal(1, _calc.Subtract(-2, -3));
     }
 
     [Fact]
     public void MultiplyTwoNumber_RuturnsProduct()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(15, calc.Multiply(5, 3));
+        Assert.Equal(15, _calc.Multiply(5, 3));
     }
     [Fact]
     public void MultiplyWithNegetiveNumber_ReturnNegetiv()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(-15, calc.Multiply(-5, 3)); 
+        Assert.Equal(-15, _calc.Multiply(-5, 3)); 
     }
 
     [Fact]
     public void DivideTwoNumbers_ReturnsQuotient()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(2, calc.Divide(6, 3));
+        Assert.Equal(2, _calc.Divide(6, 3));
     }
 
     [Fact]
     public void DivideDecimalNumbers_ReturnResult()
     {
-        var calc = new ClassLibrary1.Class1();
-        Assert.Equal(2.5, calc.Divide(5, 2));
+        Assert.Equal(2.5, _calc.Divide(5, 2));
     }
 
     [Fact]
     public void DivideByZero_ThrowsException()
     {
-        var calc = new ClassLibrary1.Class1();
 
         /// vi förvänta oss ett dividByZeroException om man försöker dela med 0
-        Assert.Throws<DivideByZeroException>(() => calc.Divide(5, 0));
+        Assert.Throws<DivideByZeroException>(() => _calc.Divide(5, 0));
     }
 }
